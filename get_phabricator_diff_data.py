d49b1df3d74517d05de2ab05f8947b4b6926c79c
Atul Madhugiri <atulm@berkeley.edu>
2022-01-03 20:52:26 -0500

    Get Phabricator diff data
    Get Phabricator diff data and write JSON to disk.

---
+++ get_phabricator_diff_data.py
@@ -0,0 +1,23 @@
+import requests
+import sys
+import json
+import os
+
+
+def get_diff_data():
+    try:
+        response = requests.get(
+            url="https://phabricator.ashoat.com/api/differential.query",
+            params={
+                "api.token": os.getenv("PHABRICATOR_API_TOKEN")
+            },
+        )
+    except requests.exceptions.RequestException:
+        sys.exit("HTTP Request failed.")
+
+    return response.json()
+
+
+phabricator_diff_data = get_diff_data()
+with open('phabricator_diffs.json', 'w') as phabricator_fd:
+    json.dump(phabricator_diff_data, phabricator_fd)
